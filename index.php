229e102d7aa7fa2a7e9568029f3f2f8ffe9f6294
shivamkumar12345 <Sk9220449@gmail.com>
2020-08-20 01:16:08 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html>
+<head><meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  </head>
+  <body>
+  <section class="my-5">
+<div><h2 class="text-center">
+प्रायोजन के लिए तूर्यनाद में आपका स्वागत है|</h2></div>
+</section>
+  <div id="demo" class="carousel slide" data-ride="carousel">
+
+<!-- Indicators -->
+
+<ul class="carousel-indicators">
+  <li data-target="#demo" data-slide-to="0" class="active"></li>
+  <li data-target="#demo" data-slide-to="1"></li>
+  <li data-target="#demo" data-slide-to="2"></li>
+  <li data-target="#demo" data-slide-to="3"></li>
+</ul>
+<!-- The slideshow -->
+<div class="carousel-inner">
+  <div class="carousel-item active">
+    <img src="img\t2018.jpg" alt="tooryanaad" width="1100" height="500">
+  </div>
+  <div class="carousel-item">
+    <img src="img\sandimg.jpg" alt="sand image" width="1100" height="500">
+  </div>
+  <div class="carousel-item">
+    <img src="img\tokshow.jpg" alt="sand image" width="1100" height="500">
+  </div>
+  <span class="carousel-control-next-icon"></span>
+  <div class="carousel-item">
+    <img src="img\kavi8.jpg" alt="Kavi sammelan" width="1100" height="500">
+  </div>
+</div>
+
+<!-- Left and right controls -->
+<a class="carousel-control-prev" href="#demo" data-slide="prev">
+  <span class="carousel-control-prev-icon"></span>
+</a>
+<a class="carousel-control-next" href="#demo" data-slide="next">
+</a>
+</div>
+<section class="my-5">
+
+<div class="container-fluid" style="background-color:steelblue">
+<div class="row">
+<div class="col-lg-6 col-md-6 col-12">
+<img src="img\logo.jpg" class="img-fluid about-img">
+</div>
+<div class="col-lg-6 col-md-6 col-12">
+<h3 class="text-center">तूर्यनाद</h3>
+<p>तूर्यनाद एक राष्ट्र-स्तरीय हिन्दी महोत्सव है, जिसका उद्देश्य राजभाषा हिन्दी व भारतीय संस्कृति के प्रचार-प्रसार द्वारा देशवासियों में राष्ट्रगौरव व आत्मगौरव की भावना का विकास करना है।यह मंच भारत भर के विश्वविद्यालयों और कॉलेजों के उत्साही, देशभक्त, बौद्धिक और ऊर्जावान युवा छात्रों के प्रतिभाओं को सामने लाता है।इसके अंतर्गत सितम्बर माह को हिन्दी माह के रूप में मनाते हुए महाविद्यालयीन विद्यार्थियों हेतु अनेक राष्ट्र स्तरीय प्रतियोगिताएँ, कार्यशालाएँ, कवि सम्मलेन आदि कार्यक्रम आयोजित होते हैं।</p>
+
+
+</div></div></div>
+</section>
+<section class="my-5">
+
+<div class="container-fluid" style="background-color:steelblue">
+<div class="row">
+
+<div class="col-lg-6 col-md-6 col-12">
+<h3 class="text-center">अतिथि विशेष</h3>
+<p>तूर्यनाद'20 का उद्घाटन समारोह 1 सितंबर को दोपहर 3 बजे ऑनलाइन के माध्यम से आयोजित किया जाएगा। उद्घाटन समारोह के मुख्य अतिथि शैलेश लोढ़ा होंगे।</p>
+
+
+</div><div class="col-lg-6 col-md-6 col-12">
+<img src="img\shailesh.jpg" class="img-fluid chief" ><h5 class="text-center">शैलेश लोढ़ा </h5>
+</div></div></div>
+</section>
+<section class="my-5">
+<div class="py-5">
+      <h2 class="text-center">चित्र प्रदर्शनी</h2>
+ <div class="container-fluid">
+ <div class="row">
+ <div class="col-lg-4 col-md-4 col-12">
+<img src="img\nukkad.jpg" class="img-fluid pb-3">
+</div>   
+<div class="col-lg-4 col-md-4 col-12">
+<img src="img\nritya.jpg" class="img-fluid pb-3">
+</div>  
+<div class="col-lg-4 col-md-4 col-12">
+<img src="img\paridhanika.jpg" class="img-fluid pb-3">
+</div>  
+<div class="col-lg-4 col-md-4 col-12">
+<img src="img\workshop.jpg" class="img-fluid pb-3">
+</div>  
+<div class="col-lg-4 col-md-4 col-12">
+<img src="img\chatrsansad.jpg" class="img-fluid pb-3">
+</div>  
+<div class="col-lg-4 col-md-4 col-12">
+<img src="img\samoh.jpg" class="img-fluid pb-3">
+</div>  
+</section>
+<section class="my-5">
+<div class="py-5">
+      <h2 class="text-center">हमारे प्रमुख प्रायोजक</h2>
+</div><div class="container-fluid">
+ <div class="row">
+ <div class="col-lg-4 col-md-4 col-12"><div class="card" >
+    <img class="card-img-top" src="img\danik.jpg" alt="Card image" style="width:100%">
+    <div class="card-body">
+      <h4 class="card-title">Danik Bhaskar</h4>
+      <a href="#" class="btn btn-primary">See Profile</a>
+    </div>
+  </div></div>
+  <div class="col-lg-4 col-md-4 col-12"><div class="card" >
+    <img class="card-img-top" src="img\made-easy.jpg" alt="Card image" style="width:100%">
+    <div class="card-body">
+      <h4 class="card-title">Made Easy </h4>
+      <a href="#" class="btn btn-primary">See Profile</a>
+    </div>
+  </div></div>
+  <div class="col-lg-4 col-md-4 col-12"><div class="card" >
+    <img class="card-img-top" src="img\toi.jpg" alt="Card image" style="width:100%">
+    <div class="card-body">
+      <h4 class="card-title">Times of India</h4>
+      <a href="#" class="btn btn-primary">See Profile</a>
+    </div>
+  </div></div>
+</div></div></div>
+</section>  
+<div  style="padding-top: 20px;padding-left:20px; background-color:black; color:#ffff" style="margin-left: 30px" style="margin-bottom: 20px">
+	<h2  style="text-align: center" "padding-bottom: 30px">तूर्यनाद की विशेषता  </h2>
+  <ul style="padding-left:5px">
+  <li> हमारे गत वर्ष के अथिति: डॉ वेद प्रताप वैदिक,  श्री मनोज श्रीवास्तव,  श्री प्रशांत त्रिपाठी, श्री मध्यम सक्सेना, श्री अखिलेन्द्र मिश्रा ।</li>
+	<li> हर त्योहार के पर्व पर उससे जुड़े रोचक किस्से और उनका महत्व और आदि जानकारी हमारे समिति के सोशल मीडिया अकाउंट पर साझा की जाती हे।</li>
+	<li> हमारे आयोजनों का प्रचार अलग-अलग शैक्षणिक संस्थाओ में जाकर, पोस्टर दवारा और सोशल मीडिया दवारा बहुत बड़े स्तर पर किया जाता हे।</li>
+  <li> 20,000+ फेसबुक अनुयायी ,2000+ इंस्टाग्राम फॉलोअर्स ,5000+ यूट्यूब सब्सक्राइबर </li>
+</ul>
+  
+	<h3  style="text-align: center" "padding-bottom: 30px"> हमारे प्रायोजक बनने के लाभ </h3>
+  <ul style="padding-left:5px;">
+	<li> हम विभिन्न सोशल मीडिया के माध्यम से आपकी कंपनी का प्रचार करेंगे |</li>
+	<li> हम अपनी वेबसाइट और पोस्टर में आपकी कंपनी का लोगो पोस्ट करेंगे |</li></ul>
+  <h4  style="text-align: center" "padding-bottom: 30px"> निवेदन </h4>
+	<p> किसी भी शिक्षण संस्थान द्वारा आयोजित पूरे देश में हिंदी का सबसे बड़ा महोत्सव तूर्यनाद इस बार पूरी तरह से ऑनलाइन होने जा रहा है। देशभर से हिंदी भाषा के बड़े-बड़े कवि इस बार हमसे ऑनलाइन जोड़ने वाले हैं। इसके साथ ही अनेकों प्रतियोगिताओं का भी आयोजन ऑनलाइन ही करवाया जा रहा है। 
+इसलिए हम आपसे अनुरोध करते हैं कि आप हमें प्रायोजित करें|</p>
+ </div> 
+ <footer class="container1 center opacity margin-bottom"  style="text-align:center" style="padding-top: 20px"  "background-color:#fff">
+  <h5 style="padding-top: 30px; color:#ffff"> प्रायोजन के लिये संपर्क करें </h5>
+  <div class="large padding-16" style="max-width:100%"> <i class="fa fa-map-marker" style="width:30px"></i> भोपाल, मध्यप्रदेश <br>
+    <i class="fa fa-phone" style="width:30px"></i> फ़ोन : +91-7089111265<br>
+    <i class="fa fa-envelope" style="width:30px"> </i>ईमेल : tooryanad@gmail.com<br>
+   <a href="https://www.facebook.com/"><i class="fa fa-facebook-official w3-hover-opacity"></i></a>
+   <a href=" https://www.instagram.com/"><i class="fa fa-instagram w3-hover-opacity"></i></a>
+   <a href="https://www.youtube.com/"><i class="fa fa-youtube w3-hover-opacity"></i></a>
+   <a href="https://twitter.com/"><i class="fa fa-twitter w3-hover-opacity"></i></a>
+    
+  </div>
+</footer>  
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
